c416a657653a09ae0777a332a5594c0ed8168dd0
MR XYZ <145436546+MR-XYZ-404@users.noreply.github.com>
2023-11-05 17:19:03 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-os.system("pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests")
+os.system("pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests;git pull;clear")
 try:
     import XYZ
 except:
